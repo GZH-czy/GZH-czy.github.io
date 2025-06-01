ac8e9f956811045383eac9cd3b59ae3ad8867d1c
GZH-czy <2679035927@qq.com>
2025-06-01 23:30:08 +0800

    Site updated: 2025-06-01 23:30:08

--- js/mouse/cursor.js
+++ js/mouse/cursor.js
@@ -40,7 +40,7 @@ class Cursor {
 
         document.body.appendChild((this.scr = document.createElement("style")));
         // 这里改变鼠标指针的颜色 由svg生成
-        this.scr.innerHTML = `* {cursor: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8' width='8px' height='8px'><circle cx='4' cy='4' r='4' opacity='.5' fill='rgb(10, 236, 255)'/></svg>") 4 4, auto}`;
+        this.scr.innerHTML = `* {cursor: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8' width='8px' height='8px'><circle cx='4' cy='4' r='4' opacity='.5' fill='rgb(6, 252, 254)'/></svg>") 4 4, auto}`;
     }
 
     refresh() {
