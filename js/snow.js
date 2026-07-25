8537d70866e92f7daae87bd995ab915ee425ee6d
GZH-czy <2679035927@qq.com>
2026-07-25 09:48:48 +0800

    Site updated: 2026-07-25 09:48:48

--- js/snow.js
+++ js/snow.js
@@ -15,7 +15,7 @@
         count: 120,
         minSize: 3,
         maxSize: 6,
-        speed: 1.5,
+        speed: 0.8,
         wind: 0.3,
         color: 'rgba(255, 255, 255, 0.9)',
         glowColor: 'rgba(180, 210, 255, 0.3)',
