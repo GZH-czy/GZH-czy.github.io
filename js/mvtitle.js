3de726e3d654b4f3d7027d9f346ac8979a66636a
GZH-czy <2679035927@qq.com>
2026-08-16 21:28:52 +0800

    Site updated: 2026-08-16 21:28:52

--- js/mvtitle.js
+++ js/mvtitle.js
@@ -19,6 +19,8 @@ document.addEventListener("visibilitychange", function () {
 // ============================================================
 //  动态彩色控制台输出
 // ============================================================
+console.clear(); // 每次清除之前的输出，产生动态效果
+
 console.log("%c子子涵ya", " text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em");
 
 console.log("%c每一个案例都是故事", " text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em");
